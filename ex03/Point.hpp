bbcb91b3db7a6294721596f872fc7a4c9390f472
Gaelle Ozon <gozon@paul-f4Ar7s8.clusters.42paris.fr>
2025-02-05 08:51:04 +0100

    Gave hpp the right name

---
+++ ex03/Point.hpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Point.hpp                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/02/05 08:36:16 by gozon             #+#    #+#             */
+/*   Updated: 2025/02/05 08:49:59 by gozon            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+class Point {
+
+    private:
+    
+        Fixed const x;
+        Fixed const y;
+    
+    public:
+        
+            Point();
+            Point(float const x, float const y);
+            Point(const Point& point);
+            Point& operator=(Point& point);
+            ~Point();
+
+        static const Fixed& scalar(Point& common, Point& a, Point& b); 
+};
