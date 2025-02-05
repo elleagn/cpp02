e2b141ae9dd863acf355d3e5f98e8991d6ef8d3e
Gaëlle Ozon <gozon@student.42.fr>
2025-02-05 18:31:25 +0100

    Replace scalar product by determinant

--- ex03/Point.cpp
+++ ex03/Point.cpp
@@ -6,56 +6,56 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 08:51:21 by gozon             #+#    #+#             */
-/*   Updated: 2025/02/05 12:13:21 by gozon            ###   ########.fr       */
+/*   Updated: 2025/02/05 18:30:19 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Point.hpp"
 
 Point::Point() : x(Fixed()), y(Fixed()) {
-    
-} 
+
+}
 
 Point::Point(float const x, float const y) : x(Fixed(x)), y(Fixed(y)) {
-    
+
 }
 
 Point::Point(const Point& point) : x(Fixed(point.x)), y(Fixed(point.y)) {
-    
+
 }
 
 Point&  Point::operator=(Point&) {
 
     return (*this);
-    
+
 }
 
 Point::~Point() {
 
 }
 
-const Fixed Point::scalar(const Point& origin, const Point& a, const Point& b) {
-    
+const Fixed Point::det(const Point& origin, const Point& a, const Point& b) {
+
     Fixed result;
 
-    result = (a.x - origin.x) * (b.x - origin.x) + (a.y - origin.y) * (b.y - origin.y);
+    result = (a.x - origin.x) * (b.y - origin.y) - (a.y - origin.y) * (b.x - origin.x);
     return (result);
 
 }
 
-bool    Point::isOnSameSide(const Point& point, const Point& lineOrigin, 
+bool    Point::isOnSameSide(const Point& point, const Point& lineOrigin,
             const Point& lineDir) const {
-                
-    Fixed zero;
-    Fixed scalar1;
-    Fixed scalar2;
 
-    scalar1 = Point::scalar(lineOrigin, lineDir, point);
-    scalar2 = Point::scalar(lineOrigin, lineDir, *this);
-    
-    if (scalar1 * scalar2 < zero) {
+    Fixed zero;
+    Fixed det1;
+    Fixed det2;
+
+    det1 = Point::det(lineOrigin, lineDir, point);
+    det2 = Point::det(lineOrigin, lineDir, *this);
+
+    if (det1 * det2 < zero) {
         return (false);
     }
     return (true);
-                
+
 }
--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,7 +6,7 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 08:36:16 by gozon             #+#    #+#             */
-/*   Updated: 2025/02/05 12:13:08 by gozon            ###   ########.fr       */
+/*   Updated: 2025/02/05 18:29:26 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,22 +16,22 @@
 class Point {
 
     private:
-    
+
         Fixed const x;
         Fixed const y;
 
-        static const Fixed scalar(const Point& origin, const Point& a, 
+        static const Fixed det(const Point& origin, const Point& a,
                                 const Point& b);
-    
+
     public:
-        
+
             Point();
             Point(float const x, float const y);
             Point(const Point& point);
             Point& operator=(Point&);
             ~Point();
 
-            bool    isOnSameSide(const Point& point, const Point& lineOrigin, 
+            bool    isOnSameSide(const Point& point, const Point& lineOrigin,
                         const Point& lineDir) const;
 
 };
