0a0893a418a11cf0bd3ff127d3c4758b67fd8114
Gaëlle Ozon <gozon@student.42.fr>
2025-02-07 10:40:27 +0100

    Add include guards

--- ex03/Point.hpp
+++ ex03/Point.hpp
@@ -6,9 +6,12 @@
 /*   By: gozon <gozon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/05 08:36:16 by gozon             #+#    #+#             */
-/*   Updated: 2025/02/05 18:29:26 by gozon            ###   ########.fr       */
+/*   Updated: 2025/02/07 10:40:17 by gozon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#ifndef POINT_HPP
+#define POINT_HPP
 
 #include "Fixed.hpp"
 #include <iostream>
@@ -35,3 +38,5 @@ class Point {
                         const Point& lineDir) const;
 
 };
+
+#endif
